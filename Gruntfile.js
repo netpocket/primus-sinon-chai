171f520d30bdf4910ce1dfaf5d711dfd504e12b8
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-02-25 05:09:23 -0800

    grunt bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,17 @@
 var fs = require('fs');
 
 module.exports = function(grunt) {
+  grunt.initConfig({
+    bump: {
+      options: {
+        files: [
+          'package.json',
+          'bower.json'
+        ],
+        pushTo: 'origin'
+      }
+    }
+  });
   grunt.loadNpmTasks('grunt-bump');
 };
 
